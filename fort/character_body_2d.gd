bbc7a103d850a806996ed4ef63ebae46d8ad51b2
Joe DiPilato <joe.dipilato@gmail.com>
2024-01-06 14:26:32 -0500

    walk

--- fort/character_body_2d.gd
+++ fort/character_body_2d.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @export var starting_direction : Vector2 = Vector2(0,0)
 
 @onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
 
 func _ready():
 	update_animation_params(starting_direction)
@@ -13,6 +14,13 @@ func update_animation_params(move_param : Vector2):
 		animation_tree.set("parameters/Idle/blend_position",move_param)
 		animation_tree.set("parameters/Walk/blend_position",move_param)
 		print(move_param)
+	set_animation_state()
+
+func set_animation_state():
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("Walk")
+	else:
+		state_machine.travel("Idle")
 
 func _physics_process(_delta):
 	var input_direction = Vector2(
