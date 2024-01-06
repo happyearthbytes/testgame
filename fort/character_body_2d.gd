2b4730cc5ba7437a6092179c2ec8b73bbb1cc846
Joe DiPilato <joe.dipilato@gmail.com>
2024-01-06 14:12:58 -0500

    move

--- fort/character_body_2d.gd
+++ fort/character_body_2d.gd
@@ -1,19 +1,25 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100
+@export var starting_direction : Vector2 = Vector2(0,0)
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var animation_tree = $AnimationTree
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _ready():
+	update_animation_params(starting_direction)
 
+func update_animation_params(move_param : Vector2):
+	if(move_param != Vector2.ZERO):
+		animation_tree.set("parameters/Idle/blend_position",move_param)
+		animation_tree.set("parameters/Walk/blend_position",move_param)
+		print(move_param)
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
 	
 	velocity = input_direction * move_speed
+	update_animation_params(input_direction)
 	move_and_slide()
