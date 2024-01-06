52634e424ca119010bc06306d719c22617072fd9
Joe DiPilato <joe.dipilato@gmail.com>
2024-01-06 12:33:05 -0500

    new move

--- fort/character_body_2d.gd
+++ fort/character_body_2d.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@export var move_speed : float = 100
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -9,17 +10,10 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var LR = Input.get_axis("ui_left", "ui_right")
-	var UD = Input.get_axis("ui_up", "ui_down")
-	if LR:
-		velocity.x = LR * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if UD:
-		velocity.y = UD * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-		
+	var input_direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	
+	velocity = input_direction * move_speed
 	move_and_slide()
