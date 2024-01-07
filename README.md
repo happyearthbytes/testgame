bdcfe20a2f3c3e077bdf84f9a103a0adda94c942
Joe DiPilato <joe.dipilato@gmail.com>
2024-01-06 19:05:42 -0500

    add pause unless direct

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 https://github.com/marketplace/actions/godot-ci
 https://opengameart.org/content/lpc-character-bases
+
+top.location.href = window.location.href; 
+window.location.assign(top.location.href)
+top.location.assign(window.location.href)
---
+++ fort/main.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+
+func pause():
+	get_tree().paused = true
+	#show()
+	#hide()
+
+func run_on_web():
+	if OS.has_feature('web'):
+		JavaScriptBridge.eval("console.log('Starting Godot Game for Web')")
+		redirect()
+	else:
+		print("Starting Godot Game Locally")
+
+func redirect():
+	var direct_link = JavaScriptBridge.eval("window.location.href;")
+	var main_page = JavaScriptBridge.eval("top.location.href;")
+	if(direct_link != main_page):
+		JavaScriptBridge.eval("alert('Visit the game at: " + direct_link + "');")
+		pause()
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	process_mode = Node.PROCESS_MODE_PAUSABLE
+	run_on_web()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
