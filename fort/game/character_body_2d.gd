221dc4059a066ace39d0de39d72d3f67a41d4910
Joe DiPilato <joe.dipilato@gmail.com>
2024-01-09 01:20:11 -0500

    add animations

--- fort/game/character_body_2d.gd
+++ fort/game/character_body_2d.gd
@@ -6,6 +6,18 @@ extends CharacterBody2D
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 
+var use_weapon : bool = false :
+	set(use):
+		use_weapon = use
+		state_machine.travel("Weapon")
+	get:
+		return use_weapon
+
+func _input(event : InputEvent):
+	if(event.is_action_pressed("use_weapon")):
+		print("A")
+		use_weapon = true
+
 func _ready():
 	update_animation_params(starting_direction)
 
@@ -13,6 +25,8 @@ func update_animation_params(move_param : Vector2):
 	if(move_param != Vector2.ZERO):
 		animation_tree.set("parameters/Idle/blend_position",move_param)
 		animation_tree.set("parameters/Walk/blend_position",move_param)
+		animation_tree.set("parameters/Weapon/blend_position",move_param)
+	use_weapon = false
 	set_animation_state()
 
 func set_animation_state():
