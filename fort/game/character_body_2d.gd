9723d59158cbe1ef71555a7d8b162a9c9b12b2bd
Joe DiPilato <joe.dipilato@gmail.com>
2024-01-09 19:48:50 -0500

    update animations

--- fort/game/character_body_2d.gd
+++ fort/game/character_body_2d.gd
@@ -26,20 +26,25 @@ func update_animation_params(move_param : Vector2):
 		animation_tree.set("parameters/Idle/blend_position",move_param)
 		animation_tree.set("parameters/Walk/blend_position",move_param)
 		animation_tree.set("parameters/Weapon/blend_position",move_param)
-	use_weapon = false
 	set_animation_state()
 
 func set_animation_state():
+	if use_weapon:
+		use_weapon=false
+		return
 	if(velocity != Vector2.ZERO):
 		state_machine.travel("Walk")
 	else:
 		state_machine.travel("Idle")
+	print(state_machine.get_current_node())
 
 func _physics_process(_delta):
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
+	).limit_length()
+	input_direction.normalized()
+
 	
 	velocity = input_direction * move_speed
 	update_animation_params(input_direction)
