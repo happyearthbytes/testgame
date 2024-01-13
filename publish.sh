8d41d7ac73d082b3b61ca8d22199512681cf7814
Joe DiPilato <joe.dipilato@gmail.com>
2024-01-13 11:59:16 -0500

    automated push

--- publish.sh
+++ publish.sh
@@ -2,3 +2,5 @@
 echo "Update the publish branch to match main"
 set -x
 git push origin --force main:publish
+echo "https://github.com/happyearthbytes/testgame/actions"
+echo "https://happyearthbytes.itch.io/fort"
