df2f31c4c79fde76c5c2d6b5aeecb5df9c434c5f
Joe DiPilato <joe.dipilato@gmail.com>
2024-01-15 12:41:18 -0500

    automated push

--- fort/game.gd
+++ fort/game.gd
@@ -1,11 +1,18 @@
 extends Node
 
-var id : ID = ID.new("game","singleton")
+var id : ID = ID.new("game","main")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 	print(id) 
+	print(id.children())
+	id.next_child("block")
+	id.next_child("block")
+	id.next_child("block")
+	print(id.children())
+	id.remove_child_inst("block.2")
+	print(id.children())
 	#var id = ID.id("xx","yy")
 	#myid = ID.new("me")
 	#print(myid)
--- fort/id.gd
+++ fort/id.gd
@@ -19,22 +19,28 @@ func _to_string() -> String:
 func _add_child(child_in: ID):
 	_children[child_in.id] = child_in
 
-func remove_child_id(child_in: String):
-	if child_in in _children:
-		print("removing")
-		print(child_in)
+func remove_child_inst(inst_name : String):
+	var child_id = "%s.%s" % [id,inst_name]
+	remove_child_id_str(child_id)
 
-func remove_child(child_in: ID):
-	remove_child_id(child_in.id)
+func remove_child_id_str(child_in: String):
+	if child_in in _children:
+		_children.erase(child_in)
+	else:
+		printerr("unable to remove ID: ", child_in)
+
+
+func remove_child_id(child_in: ID):
+	remove_child_id_str(child_in.id)
 
 func new_child(inst_in) -> ID:
-	var new_child = ID.new(mgr,inst_in)
+	var new_child = ID.new(id,inst_in)
 	_add_child(new_child)
 	return new_child
 
-func next_child() -> ID:
+func next_child(inst_name : String) -> ID:
 	_nextid += 1
-	return new_child(_nextid)
+	return new_child("%s.%d" % [inst_name,_nextid])
 
 func children() -> Dictionary:
 	return _children
